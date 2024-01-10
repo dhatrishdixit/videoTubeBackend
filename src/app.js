6d9aed7c4e6804b94dbfe9b8f0c287911a1a68dc
dhatrishdixit <dhatrish29@gmail.com>
2024-01-10 12:59:34 +0530

    new change

--- src/app.js
+++ src/app.js
@@ -11,7 +11,10 @@ app.use(express.json({
     limit:'16kb'
 }));
 app.use(cookieParser());
-app.use(express.urlencoded());
+app.use(express.urlencoded({
+    extended:true,
+    limit:'16kb'
+}));
 app.use(express.static('public'));
 
  
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+// then catch syntax 
+const asyncHandler = (requestHandler) => {
+      return (req,res,next)=>{
+          Promise.resolve(requestHandler(req,res,next)).catch(err => next(err))
+      }
+}
+
+
+
+// async -await syntax 
+// const asyncHandler = (fn) => async (req,res,next)=>{
+//       try {
+//         await fn(req,res,next)
+//       } catch (error) {
+//         // error 
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message:err.message
+//         })
+//       }
+// }
