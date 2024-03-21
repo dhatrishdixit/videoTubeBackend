ac7e7b8c4412152444182f8aa70e51c3e6afe869
dhatrishdixit <dhatrish29@gmail.com>
2024-03-22 01:05:20 +0530

    made some changes

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,10 +3,10 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from 'jsonwebtoken'
 
-export const verifyJWT = asyncHandler(async (req,_,next)=>{
+export const verifyJWT = asyncHandler(async (req,res,next)=>{
      try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","");
-   
+      //   console.log("cookie",token);
         if(!token) throw new ApiError(401,'unauthorized request')
    
         const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
