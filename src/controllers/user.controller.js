3542f81d653f2e17087217c0d4c9182254f32c99
dhatrishdixit <dhatrish29@gmail.com>
2024-06-04 11:10:45 +0530

    done some stuff

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -603,7 +603,7 @@ const getWatchHistory = asyncHandler(async(req,res)=>{
             const user = await User.aggregate([
                 {
                     $match: {
-                        _id: new mongoose.Types.ObjectId(req.user._id)
+                        _id: new mongoose.Types.ObjectId(req.user?._id)
                     }
                 },
                 {
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -89,4 +89,4 @@ userSchema.methods.generateRefreshToken = function(){
     )
 }
 
-export const User = mongoose.model('User',userSchema);``
+export const User = mongoose.model('User',userSchema);
